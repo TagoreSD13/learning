c6163e7126468a80c419cdc732397cbddeee0c4f
ShivNathJhaDakr <shivnathjhadakrsolutions@gmail.com>
2025-02-23 02:15:11 +0530

    Initial commit of student data CRUD operations

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+class Main{
+    public static void main(String[]arys){
+        String name="Tagore";
+        String age="21";
+        String salary="20000";
+        System.out.println("name;"+name);
+        System.out.println("age;"+age);
+        System.out.println("salary;"+salary);
+    }
+}
